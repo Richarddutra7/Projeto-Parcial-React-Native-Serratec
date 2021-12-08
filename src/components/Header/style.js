a008f141160021211f40d05000e80d1a35f22464
Richard Dutra <richarddutra60@gmail.com>
2021-12-08 14:24:54 -0300

    menu implementado

---
+++ src/components/Header/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    
+    containerMenu:{
+        position: "absolute",
+        width: '100%',
+        bottom: 0,
+        justifyContent:'space-around',
+        flexDirection: "row"
+    },
+
+    inicio:{
+        borderRadius: 30,
+        marginRight:15,
+        width: 40,
+        height: 40,
+    }
+    
+});
+export default styles;
+
